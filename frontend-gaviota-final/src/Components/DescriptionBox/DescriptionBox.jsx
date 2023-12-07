12ae700059a039c2f4cd40fe09068e95af0553f0
Marcelo Buongiorno <mnbuongiorno@gmail.com>
2023-12-07 19:40:48 -0300

    addComponents

---
+++ frontend-gaviota-final/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Descripcion</div>
+        <div className="descriptionbox-nav-box fade">Reseñas (48)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Descripcion muy random aquiiii </p>
+          <p>
+          E-co
+          </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
---
+++ frontend-gaviota-final/src/Components/Offers/Offers.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Offers.css'
+
+const Offers = () => {
+  return (
+    <div className='offers'>
+      <div className="offers-left">
+        <h1>Exclusivo</h1>
+        <h1>Ofertas mensuales</h1>
+        <p>SOLO SUSCRIPTORES</p>
+        <button>Suscribirme</button>
+      </div>
+    </div>
+  )
+}
+
+export default Offers
